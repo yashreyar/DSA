e9160ec317e0c378b5e472ce947d1928b5957103
iyashreyar <yashreyar5143@gmail.com>
2026-03-06 14:14:32 +0530

    Do-While loop basics done

---
+++ DoWhileLoop.java
@@ -0,0 +1,10 @@
+//dowhile loop is atleast executed once and then the condition is checked
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int n = 1;
+        do { 
+            System.out.println("Hello World");
+        } while (n!=1);
+    }
+}
