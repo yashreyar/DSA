5fb80fa6bc0bfa7130e60d1ff6b91db2d8d05d29
iyashreyar <yashreyar5143@gmail.com>
2026-03-06 14:10:33 +0530

    While loop basics done

---
+++ WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int n = 1;
+        while(n <= 10){
+            System.out.println(n);
+            n++;
+        }
+    }
+}
