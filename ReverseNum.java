bb2ae1df611452f7ad7565243d29b76ea082e359
iyashreyar <yashreyar5143@gmail.com>
2026-03-07 15:10:35 +0530

    Reversing a number using loops ques solved

---
+++ ReverseNum.java
@@ -0,0 +1,12 @@
+public class ReverseNum {
+    public static void main(String[] args) {
+        int num = 123456789;
+        int ans = 0;
+        while(num > 0){
+            int rem = num % 10;
+            ans = ans * 10 + rem;
+            num = num / 10;
+        }
+        System.out.println(ans);
+    }
+}
