393d671032c9b8dfee6e6f33f48ad380dd7f0651
iyashreyar <yashreyar5143@gmail.com>
2026-03-08 13:37:12 +0530

    Swapping two nums done

---
+++ Swap.java
@@ -0,0 +1,10 @@
+public class Swap{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a + " " + b);
+    }
+}
