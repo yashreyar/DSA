014f03a57a3368d0932353052badfaddedf2542c
iyashreyar <yashreyar5143@gmail.com>
2026-03-11 11:56:50 +0530

    Max in range function added

--- MaxInArray.java
+++ MaxInArray.java
@@ -1,12 +1,21 @@
 public class MaxInArray {
     public static void main(String[] args) {
         int[] arr = {923, 345, 75, 43, 54, 653};
-        int Max = max(arr);
+        int Max = maxRange(arr, 0, 1);
         System.out.println(Max);
     }
     static int max(int[] nums){
         int Max = nums[0];
         for (int i = 1; i < nums.length; i++) {
+            if(Max < nums[i]){
+                Max = nums[i];
+            }
+        }
+        return Max;
+    }
+    static int maxRange(int[] nums, int start, int end){
+        int Max = nums[start];
+        for (int i = start+1; i <= end; i++) {
             if(Max < nums[i]){
                 Max = nums[i];
             }
